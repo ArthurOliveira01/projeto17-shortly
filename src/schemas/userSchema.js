64c40f97704f9996cb692aa61e6492dff4d07a72
Arthur <arthurods37@gmail.com>
2023-05-22 11:13:24 -0400

    fixing a bug in the schema

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const userSchema = Joi.object({
     name: Joi.string().min(1).required(),
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    email: Joi.string().email().required(),
     password: Joi.string().min(1).required(),
     confirmPassword: Joi.string().min(1).required()
 })
