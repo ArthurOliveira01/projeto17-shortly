07cc0387cd41b281155a8085e3b6290ddc989fd1
Arthur <arthurods37@gmail.com>
2023-05-22 11:40:40 -0400

    changing the login schema

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -8,6 +8,6 @@ export const userSchema = Joi.object({
 })
 
 export const loginSchema = Joi.object({
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    email: Joi.string().email().required(),
     password: Joi.string().min(1).required(),
 })
